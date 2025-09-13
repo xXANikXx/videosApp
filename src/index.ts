f6193b708f82bb9784f9dec8568858e02d4ef3ab
ANik <annikita.and@gmail.com>
2025-09-13 21:29:08 +0500

    Add vercel.json

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 setupApp(app);
 
 // порт приложения
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // запуск приложения
 app.listen(PORT, () => {
